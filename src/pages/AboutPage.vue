ba4b68f31e743c6050db3c55a1a961eed8212385
Ehud <ehudcode@gmail.com>
2022-06-28 23:26:45 +0300

    ccc

--- src/pages/AboutPage.vue
+++ src/pages/AboutPage.vue
@@ -1,6 +1,30 @@
 <template>
     <div>
          <h1 class="title">About</h1>
+            <h4>
+                We are third year students of System information engeneering.
+                This website was developed by:
+                Eitan Chertok and Ehud Malul
+                as part of web development course.
+
+                we are well experienced with html, node js, vue.
+                We can define ourselfs as fullstack developers.
+                other project we have developed in this course are:
+                <ol>
+                    <li>third age website</li>
+                    <li>pacman game using dhtml and js</li>
+                </ol>
+
+                Thank you for visit our website.
+            </h4>
+
+            <h4>
+                This website is the best food website in the world!!
+                We are good at marketing..but this time we dont have to do
+                nothing, check by yourselfs.
+            </h4>
+
+
     </div>
 </template>
 
